6a50fb6e3b4469045a7420b477d7bb4cb29d085f
M.Zayniddin <93776172+MZayniddinov@users.noreply.github.com>
2022-12-05 08:19:28 +0500

    Started footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-
+import c from './Footer.module.css'
+import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <h1>Footer</h1>
+        <div className={c["footer-info"]}>
+          <div>
+            <strong className={c["footer-info-title"]}>Information</strong>
+            <ul>
+              <li>
+                <Link className={c["footer-info-link"]} to="/">About Us</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <strong className={c["footer-info-title"]}>Contact Us</strong>
+          </div>
+          <div>
+            <strong className={c["footer-info-title"]}>Our Social Networks</strong>
+          </div>
+        </div>
+        <div className={c["footer-text"]}>
+        <p>
+          2022 Internet-shop oceanbooks.uz: We can give you opportunity download Online Books faster and with higher quality only with me. All rights reserved.
+        </p>
+      </div>
       </div>
     </footer>
   )
