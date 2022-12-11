88271011d1b3608bf46d28cbc6c7517d908499cf
M.Zayniddin <93776172+MZayniddinov@users.noreply.github.com>
2022-12-11 22:29:56 +0500

    Refactured

---
+++ src/routes/filter/Filter.jsx
@@ -0,0 +1,16 @@
+import c from './Filter.module.css';
+
+const Filter = () => {
+  return (
+    <div className={c["filter"]}>
+      <div className="container">
+        <h2 className={c["filter-title"]}> </h2>
+        <div className={c["result-wrapper"]}>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Filter
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 
 // pages
-import Home from "./Home/Home";
+import Home from "./home/Home";
 import Detail from './detail/Detail';
+import Filter from './filter/Filter';
 
 const index = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/detail/:id' element={<Detail/>}/>
+      <Route path='/explore/:id' element={<Filter/>}/>
     </Routes>
   )
 }
